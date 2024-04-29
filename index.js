99a3b808db801295e90bb91cf25e85f728c4ed7b
Cha Krupka <chakrupka238@gmail.com>
2024-04-29 13:30:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+app.use(cors);
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Response OK</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

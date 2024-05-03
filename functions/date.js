26d352e9c6ae22513dddb66fb544068fe333d06e
Cha Krupka <chakrupka238@gmail.com>
2024-05-03 10:00:48 -0400

    fixed date

--- functions/date.js
+++ functions/date.js
@@ -1,16 +1,10 @@
+import { DateTime } from "luxon";
+
 const newDate = () => {
-  var currentDate = new Date();
-  var day = currentDate.getDate().toString();
-  var month = (currentDate.getMonth() + 1).toString();
-  var year = currentDate.getFullYear().toString();
-  if (day < 10) {
-    day = `0${day}`;
-  }
-  if (month < 10) {
-    month = `0${month}`;
-  }
-  var formattedDate = `${year}-${month}-${day}`;
-  return formattedDate;
+  const dt = DateTime.now().setZone("America/New_York");
+  return dt.toFormat("yyyy-LL-dd");
 };
+
+console.log(newDate());
 
 export default newDate;

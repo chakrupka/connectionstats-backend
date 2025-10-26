a3c69ae61f65d958cb252f9997d0f1ea52176d96
Cha Krupka <chakrupka238@gmail.com>
2025-10-26 18:07:06 -0400

    update packages

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-import express from "express";
-import "express-async-errors";
-import cors from "cors";
-import config from "./utils/config.js";
-import logger from "./utils/logger.js";
-import middleware from "./utils/middleware.js";
-import mongoose from "mongoose";
-import gamesRouter from "./controllers/games.js";
-import usersRouter from "./controllers/users.js";
-import loginRouter from "./controllers/login.js";
-import statsRouter from "./controllers/stats.js";
-import topRouter from "./controllers/top.js";
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import config from './utils/config.js';
+import logger from './utils/logger.js';
+import middleware from './utils/middleware.js';
+import mongoose from 'mongoose';
+import gamesRouter from './controllers/games.js';
+import usersRouter from './controllers/users.js';
+import loginRouter from './controllers/login.js';
+import statsRouter from './controllers/stats.js';
+import topRouter from './controllers/top.js';
 
 const app = express();
 
-mongoose.set("strictQuery", false);
-logger.info("connecting to", config.MONGODB_URI);
+mongoose.set('strictQuery', false);
+logger.info('connecting to', config.MONGODB_URI);
 mongoose
   .connect(config.MONGODB_URI)
   .then((result) => {
-    logger.info("connected to MongoDB");
+    logger.info('connected to MongoDB');
   })
   .catch((error) => {
-    logger.error("error connecting to MongoDB:", error.message);
+    logger.error('error connecting to MongoDB:', error.message);
   });
 
 app.use(express.json());
 app.use(cors());
 app.use(middleware.requestLogger);
 
-app.use("/api/games", gamesRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/login", loginRouter);
-app.use("/api/stats", statsRouter);
-app.use("/api/top", topRouter);
+app.use('/api/games', gamesRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
+app.use('/api/stats', statsRouter);
+app.use('/api/top', topRouter);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Response OK</h1>');
+});
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
-
-app.get("/", (req, res) => {
-  res.send("<h1>Response OK</h1>");
-});
 
 export default app;
